9d62af9643e946ea678cd28bc2e7284fa8fe14f6
hoangunity <hoangdm.141196@gmail.com>
2021-02-14 22:36:15 +0700

    JS - Progress Bar (Dynamic Width)

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const fullscreenBtn = document.querySelector('.fullscreen');
 
 function showPlayIcon() {
   playBtn.classList.replace('fa-pause', 'fa-play');
-  playBtn.setAttribute('title', 'Play');
 }
 
 function togglePlay() {
@@ -32,6 +31,11 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Update progress bar as video plays
+function updateProgress() {
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
+
 // Volume Controls --------------------------- //
 
 // Change Playback Speed -------------------- //
@@ -41,3 +45,5 @@ video.addEventListener('ended', showPlayIcon);
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
